4e51dd7c5414f839893e8024724e34ac7e04d6c5
Lau Llobet <lau.llobet@gmail.com>
2018-04-27 17:11:15 +0200

    step

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -26,30 +26,23 @@ public class TennisGame1 implements TennisGame {
             switch (m_score1)
             {
                 case 0:
-                        score = "Love-All";
-                    break;
+                         return "Love-All";
                 case 1:
-                        score = "Fifteen-All";
-                    break;
+                        return  "Fifteen-All";
                 case 2:
-                        score = "Thirty-All";
-                    break;
+                        return "Thirty-All";
                 default:
-                        score = "Deuce";
-                    break;
-                
+                        return "Deuce";
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) return "Advantage player1";
+            else if (minusResult ==-1) return "Advantage player2";
+            else if (minusResult>=2) return "Win for player1";
+            return "Win for player2";
         }
-        else
-        {
             for (int i=1; i<3; i++)
             {
                 if (i==1) tempScore = m_score1;
@@ -69,7 +62,6 @@ public class TennisGame1 implements TennisGame {
                         score+="Forty";
                         break;
                 }
-            }
         }
         return score;
     }
