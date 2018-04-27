41c167245fcbd46933f0628ace10925e11593480
Lau Llobet <lau.llobet@gmail.com>
2018-04-27 17:14:08 +0200

    step

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -23,46 +23,59 @@ public class TennisGame1 implements TennisGame {
         int tempScore=0;
         if (m_score1==m_score2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                         return "Love-All";
-                case 1:
-                        return  "Fifteen-All";
-                case 2:
-                        return "Thirty-All";
-                default:
-                        return "Deuce";
-            }
+            return tieCases();
         }
         if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) return "Advantage player1";
-            else if (minusResult ==-1) return "Advantage player2";
-            else if (minusResult>=2) return "Win for player1";
-            return "Win for player2";
+            return overFourCases();
         }
-            for (int i=1; i<3; i++)
+        return notTieAndUnderFour(score);
+    }
+
+    private String notTieAndUnderFour(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-        }
+                case 0:
+                    score+="Love";
+                    break;
+                case 1:
+                    score+="Fifteen";
+                    break;
+                case 2:
+                    score+="Thirty";
+                    break;
+                case 3:
+                    score+="Forty";
+                    break;
+            }
+    }
         return score;
+    }
+
+    private String overFourCases() {
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) return "Advantage player1";
+        else if (minusResult ==-1) return "Advantage player2";
+        else if (minusResult>=2) return "Win for player1";
+        return "Win for player2";
+    }
+
+    private String tieCases() {
+        switch (m_score1)
+        {
+            case 0:
+                     return "Love-All";
+            case 1:
+                    return  "Fifteen-All";
+            case 2:
+                    return "Thirty-All";
+            default:
+                    return "Deuce";
+        }
     }
 }
