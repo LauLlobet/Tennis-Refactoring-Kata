16584ff4d87d2e637e44f08c6ed3aaa97d00697a
Lau Llobet <lau.llobet@gmail.com>
2018-04-27 17:36:04 +0200

    step

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,21 @@
+import java.util.Hashtable;
 
 public class TennisGame1 implements TennisGame {
-    
+
+    public static final int LOVE_CASE = 0;
+    public static final int INT = 1;
+    public static final int FIFTEEN_ALIAS = INT;
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private Hashtable<Integer, String> aliasOfScores = new Hashtable<Integer, String>(){{
+        put(0,"Love");
+        put(1,"Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
+
+    }};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -33,48 +44,17 @@ public class TennisGame1 implements TennisGame {
     private String notTieAndUnderFour() {
         String score = "";
         int tempScore = 0;
-        for (int i = 1; i<3; i++)
-        {
-            if (i==1) {
-                tempScore = m_score1;
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-            else {
-                score+="-";
-                tempScore = m_score2;
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        tempScore = m_score1;
+        score = getScoreAlias(score, tempScore);
+        score+="-";
+        tempScore = m_score2;
+        score = getScoreAlias(score, tempScore);
 
-    }
         return score;
+    }
+
+    private String getScoreAlias(String score, int tempScore) {
+        return score += aliasOfScores.get(tempScore);
     }
 
     private String overFourCases() {
