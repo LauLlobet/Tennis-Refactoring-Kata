1097f62f73e1cbebb1b0250420238dc3eeb61fa7
Lau Llobet <lau.llobet@gmail.com>
2018-05-02 11:57:55 +0200

    step

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -2,6 +2,13 @@ import java.util.Hashtable;
 
 public class TennisGame1 implements TennisGame {
 
+    public static final String ADVANTAGE_TEXT = "Advantage ";
+    public static final String WIN_FOR_TEXT = "Win for ";
+    public static final String HYPHE_TEXT = "-";
+    public static final String DEUCE_TEXT = "Deuce";
+    public static final String PLAYER2_TEXT = "player2";
+    public static final String PLAYER1 = "player1";
+    public static final String PLAYER1_TEXT = "player1";
     private int player1Score = 0;
     private int player2Score = 0;
 
@@ -17,7 +24,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1") {
+        if (playerName == PLAYER1_TEXT) {
             player1Score += 1;
             return;
         }
@@ -38,29 +45,29 @@ public class TennisGame1 implements TennisGame {
 
     private String notTieAndUnderFour(int firstPlayerScore,int secondPlayerScore) {
         return  aliasOfScores.get( firstPlayerScore) +
-                "-" + aliasOfScores.get(secondPlayerScore);
+                HYPHE_TEXT + aliasOfScores.get(secondPlayerScore);
     }
 
 
     private String overFourCases() {
         int minusResult = Math.abs(player1Score - player2Score);
         if(minusResult == 1){
-            return "Advantage "+ playerNameWithMorePoints();
+            return ADVANTAGE_TEXT + playerNameWithMorePoints();
         }
-        return "Win for " + playerNameWithMorePoints();
+        return WIN_FOR_TEXT + playerNameWithMorePoints();
 
     }
 
     private String playerNameWithMorePoints() {
         if(player1Score > player2Score){
-            return "player1";
+            return PLAYER1_TEXT;
         }
-        return "player2";
+        return PLAYER2_TEXT;
     }
 
     private String tieCases(Integer bothPlayersScore) {
         if(bothPlayersScore >= 3){
-            return "Deuce";
+            return DEUCE_TEXT;
         }
         return aliasOfScores.get(bothPlayersScore)+"-All";
    }
