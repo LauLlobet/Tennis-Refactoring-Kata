a3ae017c7154a6797243d9cd55fa78a6d72f1971
Lau Llobet <lau.llobet@gmail.com>
2018-05-02 11:53:13 +0200

    step

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -2,13 +2,9 @@ import java.util.Hashtable;
 
 public class TennisGame1 implements TennisGame {
 
-    public static final int LOVE_CASE = 0;
-    public static final int INT = 1;
-    public static final int FIFTEEN_ALIAS = INT;
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int player1Score = 0;
+    private int player2Score = 0;
+
     private Hashtable<Integer, String> aliasOfScores = new Hashtable<Integer, String>(){{
         put(0,"Love");
         put(1,"Fifteen");
@@ -18,40 +14,48 @@ public class TennisGame1 implements TennisGame {
     }};
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == "player1") {
+            player1Score += 1;
+            return;
+        }
+        player2Score += 1;
     }
 
     public String getScore() {
-        if (m_score1==m_score2)
+        if (player1Score == player2Score)
         {
-            return tieCases(m_score1);
+            return tieCases(player1Score);
         }
-        if (m_score1>=4 || m_score2>=4)
+        if (player1Score >=4 || player2Score >=4)
         {
             return overFourCases();
         }
-        return notTieAndUnderFour(m_score1,m_score2);
+        return notTieAndUnderFour(player1Score, player2Score);
     }
 
     private String notTieAndUnderFour(int firstPlayerScore,int secondPlayerScore) {
-        return  aliasOfScores.get( firstPlayerScore) + "-" + aliasOfScores.get(secondPlayerScore);
+        return  aliasOfScores.get( firstPlayerScore) +
+                "-" + aliasOfScores.get(secondPlayerScore);
     }
 
 
     private String overFourCases() {
-        int minusResult = m_score1-m_score2;
-        if (minusResult==1) return "Advantage player1";
-        else if (minusResult ==-1) return "Advantage player2";
-        else if (minusResult>=2) return "Win for player1";
-        return "Win for player2";
+        int minusResult = Math.abs(player1Score - player2Score);
+        if(minusResult == 1){
+            return "Advantage "+ playerNameWithMorePoints();
+        }
+        return "Win for " + playerNameWithMorePoints();
+
+    }
+
+    private String playerNameWithMorePoints() {
+        if(player1Score > player2Score){
+            return "player1";
+        }
+        return "player2";
     }
 
     private String tieCases(Integer bothPlayersScore) {
