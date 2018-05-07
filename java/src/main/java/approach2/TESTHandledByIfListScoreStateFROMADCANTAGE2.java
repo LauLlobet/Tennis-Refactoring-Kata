ba860fdf4129b3430b2db4891496d3a6597204bb
Lau Llobet <lau.llobet@gmail.com>
2018-05-07 09:59:00 +0200

    end of attempt

---
+++ java/src/main/java/approach2/TESTHandledByIfListScoreStateFROMADCANTAGE2.java
@@ -0,0 +1,6 @@
+package approach2;
+
+import approach2.states.TESTHandledByIfListScoreState;
+
+public class TESTHandledByIfListScoreStateFROMADCANTAGE2 extends TESTHandledByIfListScoreState {
+}
---
+++ java/src/main/java/approach2/TESTHandledByIfListScoreStateFROMWINP1.java
@@ -0,0 +1,6 @@
+package approach2;
+
+import approach2.states.TESTHandledByIfListScoreState;
+
+public class TESTHandledByIfListScoreStateFROMWINP1 extends TESTHandledByIfListScoreState{
+}
