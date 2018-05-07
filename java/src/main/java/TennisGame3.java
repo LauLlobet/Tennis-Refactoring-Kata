b03777d3490c3f10f4214462ddfafef1efcdab7a
Lau Llobet <lau.llobet@gmail.com>
2018-05-07 14:57:26 +0200

    lost time trying a too big step

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -13,7 +13,7 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (CallPoint.isCallable(p1) && p2 < 4 && !(p1 + p2 == 6)) {
+        if (CallPoint.isCallable(p1) && CallPoint.isCallable(p2) && !(p1 + p2 == 6)) { // too large step
             return tieCall();
         } else {
             if (p1 == p2)
